6ecd33c58ecc9cdc5faa19b236c5e5f82d9970ce
Marco Descher <marco@descher.at>
2018-04-06 13:46:45 +0200

    Further development

--- es-sof-sample-app/src/App.vue
+++ es-sof-sample-app/src/App.vue
@@ -14,7 +14,7 @@
     <footer class="footer">
       <span class="text-muted">
         <div v-if="this.$parent.smart != null">
-         Connected to {{ this.$parent.smart.server.serviceUrl}} [<a href="#" v-on:click="disconnect()">disconnect</a>]
+         Connected to {{ this.$parent.smart.server.serviceUrl}} [<a href="#" v-on:click="tokenInfo()">token-info</a>] [<a href="#" v-on:click="disconnect()">disconnect</a>]
         </div>
         <div v-else class="container">
         Not connected
@@ -30,6 +30,11 @@ export default {
   methods: {
     disconnect () {
       this.$parent.smart = null
+    },
+    tokenInfo () {
+      var winPrint = window.open('', '', 'left=0,top=0,width=800,height=600,toolbar=0,scrollbars=0,status=0')
+      winPrint.document.write('<pre>' + JSON.stringify(this.$parent.smart, null, 2) + '</pre>')
+      winPrint.document.close()
     }
   }
 }
