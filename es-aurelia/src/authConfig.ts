b32a08f82de9fc7035d1323d6478e122b65c10b3
gerry <weirich@elexis.ch>
2018-04-24 21:55:39 +0200

    google login works

--- es-aurelia/src/authConfig.ts
+++ es-aurelia/src/authConfig.ts
@@ -1,7 +1,10 @@
-export default{
+export default {
   endpoint: 'auth',
-  configureEndpoints:['auth'],
-  google:{
-    clientID: "goooogle"
+  configureEndpoints: ['auth'],
+  providers: {
+    google: {
+      clientId: "510355454208-ggh8a9dvo76q0bbsnjbjdnv21d83siv1.apps.googleusercontent.com",
+      redirectUri: "http://localhost:9000/auth/google/callback"
+    }
   }
 }
